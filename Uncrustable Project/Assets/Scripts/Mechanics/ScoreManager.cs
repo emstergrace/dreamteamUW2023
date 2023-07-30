8aeef81f2f732bcd9615e4c2cdbff3cd065b66ba
sgrabar <grabar.svetlana@gmail.com>
2023-07-30 13:26:41 -0700

    Score Manager update
    Added logic to score manager
    Added UI for scores. It should scale no matter the resolution
    
    the high score and score aren't updated yet so they'll stay at 0 until we add logic for collecting items

--- Uncrustable Project/Assets/Scripts/Mechanics/ScoreManager.cs
+++ Uncrustable Project/Assets/Scripts/Mechanics/ScoreManager.cs
@@ -1,25 +1,55 @@
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour
 {
 
-    public int score;
-    public List<int> highScores = new List<int>(); //the first 5 items of this list will be displayed. Undecided if it should save more than that
-    public Text scoreText;
+    public int score; // this is the score for the current game
+    public int highScore; // temporarily testing with just one high score stored, instead of 5
+    //public List<int> highScores = new List<int>(); //the first 5 items of this list will be displayed. Undecided if it should save more than that
+    public TextMeshProUGUI scoreText, highScoreText;
 
-    public void Update()
+
+    private void Start()
     {
-        scoreText.text = score.ToString();
+        UpdateScoreText();
+        UpdateHighScoreText();
     }
 
+    // prob a cleaner way to do this part.
     public void AddScore(int amount)
     {
         score += amount;
+        UpdateHighScore();
     }
     public void SubtractScore(int amount)
     {
         score -= amount;
+        UpdateHighScore();
+    }
+
+    public void UpdateHighScore()
+    {
+        if (score > PlayerPrefs.GetInt("HighScore", 0))
+        {
+            /* this will save the high score on the user's computer, so it is saved between playthroughs. If the player uses a different computer, however,
+             * they will not be able to see their previous scores */
+            PlayerPrefs.SetInt("HighScore", score);
+
+            // update the high score text dynamically
+            UpdateHighScoreText();
+        }
+    }
+
+    public void UpdateHighScoreText()
+    {
+        highScoreText.text = $"HighScore: {PlayerPrefs.GetInt("HighScore", 0)}";
+    }
+
+    public void UpdateScoreText()
+    {
+        scoreText.text = $"Score: {score, 0}";
     }
 }
