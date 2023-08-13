c15d477a31a1c970a9b03e3682ac18f9837143a2
sgrabar <grabar.svetlana@gmail.com>
2023-08-13 14:15:50 -0700

    High Score page (not populated) plus made bad mushrooms reduce score more

--- Uncrustable Project/Assets/Scripts/UI/Menu.cs
+++ Uncrustable Project/Assets/Scripts/UI/Menu.cs
@@ -8,6 +8,11 @@ public class Menu : MonoBehaviour
         ScoreVariables.totalScore = 0;
         SceneManager.LoadScene("Levels/Scenes/UPDATEDLEVELONE");
     }
+    public void OnHighScoreButton()
+    {
+        SceneManager.LoadScene("UI/Scenes/High Scores");
+    }
+
     public void OnQuitButton()
     {
         Application.Quit();
