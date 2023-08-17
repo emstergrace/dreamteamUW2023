84e4778f9a2b1d0dbbdbec31afbef097f3c9ad5b
OnlyFeatures <soren.j.ludwig@gmail.com>
2023-08-16 17:21:49 -0700

    dsfajsd

--- Uncrustable Project/Assets/Scripts/Gameplay/PlayerEnemyCollision.cs
+++ Uncrustable Project/Assets/Scripts/Gameplay/PlayerEnemyCollision.cs
@@ -26,6 +26,9 @@ namespace Platformer.Gameplay
                 var childController = child.GetComponent<ChildController>();
                 childController.CaughtByWitch();
                 Debug.Log("Child eaten while following the player.");
+
+                witch.ResetMovementSpeed();
+                witch.WaitForPreyToRun();
             }
             else
             {
